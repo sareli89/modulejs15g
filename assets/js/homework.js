99489d51fc452653bc35f6a1cfdf7cb96b786ce0
Sareli <sareli89@users.noreply.github.com>
2021-12-06 20:44:10 -0500

    tarea clase 9

--- assets/js/homework.js
+++ assets/js/homework.js
@@ -1,109 +1,38 @@
+/** 
+ * Dado un arreglo con nombres de personas, 
+ * devuele un arreglo con los nombres de las personas que empiezan con vocales (capitalizados)
+ * onlyNamesVowels( ['jorge','ana','ivan','sergio','luis','oscar' ] )
+ * -> ['Ana','Ivan','Oscar']
+*/
 
-   
+let onlyVowelsName  = (arr) => {
+    
+    // let vowels = /[AEIOU]/
+    return arr.map ( word =>  word.slice(0,1).toUpperCase() + word.slice(1).toLowerCase())
+    .filter(word => word[0] === 'A' && word[0] === 'E' && word[0] === 'I' && word[0] === 'O' && word[0] === 'U')
+    console.loglog(word)
+} 
 
-/**
- * arr.forEach()
- * Dado el siguiente array 
- * ['string','value','min','asdfghjk']
- * Hacer una funcion que devuelva el string con mas caracteres
- * 
- */
-
-// let largestString = ['string','value','min','asdfghjk']
-
-// const mostLarge  = (arr) => {
-//     let moreString = ''
-
-//     // moreString = mostLarge
-//     arr.forEach(element => {
-//         if(moreString.length < element.length) {
-//             moreString = element;
-//         }
-//     })
-//     return moreString;
-// }
-// console.log( mostLarge( ['string','value','min','asdfghjk']))
-
+let toUpper = onlyVowelsName(['jorge','ana','ivan','sergio','luis','oscar' ])
+console.log(toUpper)
 
 
 /**
- * arr.map()
- * Dado un array 
- * Hacer una funcion que devuelva el mismo array
- * Pero con los items al reves
- * P.ej. reverseStringArr( ['string','value','min'] )
- * Salida: ['gnirts','eulav','nim']
+ * 
+ * Dado una arreglo compuesto por arreglos, 
+ * crea una funcion que calcule la suma de los arreglos y posteriormente la suma de las sumas
+ * additionMultiArr(  [	[1,2,3] , [1,3,2] , [3,2,1] ] )
  * 
  */
+ 
+ 
+let sumAr = 0, totalSum = 0
 
-// let strToReverse = ['string','value','min']
-
-// const reverseStringArr = arr => {
-//     let reverse = []
-//     reverse = arr.map( element => {
-//         for (let i = element.length - 1; i >= 0; i--) {
-//             reverse += element[i]
-//         }
-//         return reverse
-//     })
-//     console.log(reverse)
-// }
+let sum_ArArr = (arr) => {
+    return arr.map(val => val.reduce( (acc, value) => acc += value)).reduce((sum,total) => sum += total)
+}
+let array = sum_ArArr( [ [1,2,3] , [1,3,2] , [3,2,1] ] )
+console.log(array)
 
 
-// let moreString = []
-
-// // moreString = mostLarge
-
-// });
-
-/**
- * arr.filter(), .map()
- * Funcion que reciba un array de strings y numeros
- * Filtre solo los strings
- * luego, Capitalice cada string
- * luego, filtre solo los que tengan mas de 5 caracteres
- * y al final, los que tengan al menos 2 letras "a" 
- * 
- * p.ej.  filterStrings( ['guadalajara', 3, 'caracas', 'Oslo', 'brasil', 0] )
- * Salida -> ['Guadalajara', 'Caracas']
- */
-
-// let array = ['guadalajara', 3, 'caracas', 'Oslo', 'brasil', 0]
-
-// const filterStrings  = (arr) => {
-//     return arr.filter(word => typeof word === 'string')
-//     .map ( word =>  word.slice(0,1).toUpperCase() + word.slice(1).toLowerCase())
-//     .filter( word =>  word.length > 5)
-//     .filter( word =>  word.split('a').length >= 3)
-// }
-
-// const filterStrings  = (arr) => {
-//     return arr.filter(word => typeof word === 'string')
-//     .map ( word => {
-//         return word.slice(0,1).toUpperCase() + word.slice(1).toLowerCase()
-//         console.log(word)
-//     })
-//     .filter( word => {
-//         return word.length > 5
-//     })
-//     .filter ( word => {
-//         return word.split('a').length >= 3
-//     })
-// }
-
-
-const fullname = ['jorge','luis', 'camarillo', 'cristobal']
-// ->'jlcc'
-
-
-// let getInitials = (arr) => {
-//     return arr.reduce( (acc, cv) => `${acc}${cv.slice(0,1)}`,'')
-// }
-
-let getInitials = (arr) => arr.reduce( (acc, cv) => `${acc}${cv.slice(0,1)}`,'')
-
-
-let initials = getInitials(fullname)
-console.log(initials)
-
-
+ 
